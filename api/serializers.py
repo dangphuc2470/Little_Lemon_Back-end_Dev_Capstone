7d585710053058f13126e9fafd732e647253108f
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2023-07-27 22:06:58 +0700

    Update

--- api/serializers.py
+++ api/serializers.py
@@ -4,15 +4,13 @@ from restaurant.models import Menu, Booking
 
 
 class BookingSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Booking
-        fields = ['name', 'no_of_guests', 'booking_date']
+        fields = ["name", "no_of_guests", "booking_date"]
 
 
 class MenuSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Menu
-        fields = ['id', 'title', 'price', 'inventory']
-        read_only = ['id']
+        fields = ["id", "title", "price", "inventory"]
+        read_only = ["id"]
